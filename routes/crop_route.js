2a0f6b1c857ccca45e9563088203473d3e4d703b
Gabriel <gabriel@healthcloud.co.ke>
2022-04-13 08:37:28 +0300

    feat : add dynamic querying of data

--- routes/crop_route.js
+++ routes/crop_route.js
@@ -4,11 +4,11 @@ const dbConnection = require('../db/connection');
 
 const cropRoute = express.Router();
 
-cropRoute.route("/").get(async function(req,res){
+cropRoute.route("/:cropName").get(async function(req,res){
     const db = dbConnection.getDb();
     db
     .collection('crops')
-    .find({cropName:"Beans"})
+    .find({cropName:req.params.cropName})
     .toArray()
     .then((result)=>{
         res.json(result)
